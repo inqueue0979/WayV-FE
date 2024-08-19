fdd069d0076940a6bf65d81a6db568f79a7f2425
inqueue0979 <inqueue0979@gmail.com>
2024-08-20 05:10:04 +0900

    Deleted NMD.js

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
@@ -5,7 +6,7 @@ export default function Navbar() {
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="wayv.png" className="h-8" alt="WayV 로고" />
+                <Image src="wayv.png" className="h-8" alt="WayV 로고" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">WayV 4.20</span>
             </Link>
             <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
