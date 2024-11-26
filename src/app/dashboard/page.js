62d6956457fd7288055368ad53bd7731acda596e
inqueue0979 <inqueue0979@gmail.com>
2024-11-27 05:58:21 +0900

    Changing Dashboard Page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -9,33 +9,14 @@ export default function Home() {
   const [link, setLink] = useState("");  // Input의 값을 관리하는 상태
 
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <div className="flex flex-col items-center justify-center">
-        <div className="bg-gradient-to-r from-blue-300 to-green-300 p-4 rounded-3xl mb-1">
-          <Image src="/wayv.png" alt="WayV 로고" width={100} height={100} />
-        </div>
-        <p className="Pretendard-Bold text-xl">웹브릿지 프로젝트</p>
-        <p className="text-sm"><strong className="Pretendard-Bold">웹 접근성</strong> 검사 툴킷</p>
-        <div className="mt-2">
-          <p className="text-md m-4 text-center">
-            <strong className="Pretendard-Bold">링크 입력 한 번</strong>으로<br></br><strong className="Pretendard-Bold">웹 접근성</strong>을 확인해 보세요!
-          </p>
-        </div>
-
-        <div className="flex w-full max-w-xl items-center space-x-2">
-          <Input
-            id="Link"
-            placeholder="링크 입력 (HTTP/HTTPS 명시)"
-            value={link}
-            onChange={(e) => setLink(e.target.value)}  // Input 값 업데이트
-          />
-          <Link
-            href={`/solution?url=${encodeURIComponent(link)}`}
-            className="bg-gray-800 text-white py-2 px-4 rounded-lg w-20 text-center hover:bg-gray-700"
-          >
-            <p className="text-md">제출</p>
-          </Link>
-        </div>
+    <main className="min-h-screen max-w-screen-xl mt-28 mx-auto p-3">
+      <h1>프로젝트 둘러보기</h1>
+      <p className="text-gray-500 mt-0 mb-6">웹 접근성 프로젝트를 한 눈에 보고, 추가하거나 관리할 수 있습니다.</p>
+      <div className="grid grid-cols-4 flex-col items-center justify-center">
+        <p>프로젝트 (구현 중)</p>
+        <p>프로젝트</p>
+        <p>프로젝트</p>
+        <p>프로젝트</p>
       </div>
     </main>
   );
